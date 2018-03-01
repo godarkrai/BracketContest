2a2832526f16f2af7afa903e833487b79b0aaaf8
FO-nTTaX <fo.nttax@yahoo.de>
2018-03-01 16:34:40 +0100

    code style

--- api/Connection.php
+++ api/Connection.php
@@ -2,31 +2,31 @@
 
 class Connection {
 
-    protected static $db;
+	protected static $db;
 
-    private function __construct() {
-        global $wgBracketContestDbType,
-            $wgBracketContestDbServer,
-            $wgBracketContestDbName,
-            $wgBracketContestDbUser,
-            $wgBracketContestDbPassword;
-        
-        try {
-            self::$db = new PDO( $wgBracketContestDbType . ':host=' . $wgBracketContestDbServer. ';dbname=' . $wgBracketContestDbName,
-                $wgBracketContestDbUser, $wgBracketContestDbPassword);
-            self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-        }
-        catch (PDOException $e) {
-            // echo "Connection Error: " . $e->getMessage();
-        }
+	private function __construct() {
+		global $wgBracketContestDbType,
+			$wgBracketContestDbServer,
+			$wgBracketContestDbName,
+			$wgBracketContestDbUser,
+			$wgBracketContestDbPassword;
+		
+		try {
+			self::$db = new PDO( $wgBracketContestDbType . ':host=' . $wgBracketContestDbServer. ';dbname=' . $wgBracketContestDbName,
+				$wgBracketContestDbUser, $wgBracketContestDbPassword);
+			self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+		}
+		catch (PDOException $e) {
+			// echo "Connection Error: " . $e->getMessage();
+		}
 
-    }
+	}
 
-    public static function getConnection() {
-        if (!self::$db) {
-            new Connection();
-        }
-        return self::$db;
-    }
+	public static function getConnection() {
+		if (!self::$db) {
+			new Connection();
+		}
+		return self::$db;
+	}
 }
 ?>
